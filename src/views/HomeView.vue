be4b4fee53729e32a3ed6c213ec34b8780a197e1
LKANIT Mohamed Taib <mohamed.lkanit@um6p.ma>
2025-04-10 19:08:49 +0100

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,42 @@
 <template>
   <div>
-    <button @click="addDocument">Add Document</button>
+    <button @click="addADoc">Add Document</button>
+    <button @click="readDocs">Read Documents</button>
   </div>
 </template>
 
 <script setup>
-import {db} from '@/Firebase/Config'
-import {collection , addDoc} from 'firebase/firestore'
+import { db } from '@/Firebase/Config'
 
-const AddDocument = async () => {
-  try {
-  }
-
-  catch{
-    
-  }
-
+function readDocs() {
+  db.collection("Discussions")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} =>`, doc.data());
+      });
+    })
+    .catch((error) => {
+      console.error("Error reading documents:", error);
+    });
 }
 
+async function addADoc() {
+  try{
+    let docRef = await db.collection("Discussions").add({
+      Contenu: "Ceci est un contenu d'exemple",
+      Titre: "Titre d'exemple",
+      editeur: "Utilisateur1",
+      estResponse: false,
+      responses: ["Réponse 1", "Réponse 2"],
+    })
 
+    console.log(`Document id added is : ${docRef.id}`)
 
+  }
+  catch{
+    console.log("There was an error!");
+
+  }
+}
 </script>
